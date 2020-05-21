6a1ba8b853fbc496089d162d367fbaddb52620ef
Erol Ege Saglam <egesaglam702@gmail.com>
2020-05-21 04:22:02 +0300

    Postgresql eklendi

--- udemy/udemy/settings.py
+++ udemy/udemy/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'udemy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER' : 'postgres',
+        'PASSWORD' : 'toor',
+        'HOST' : 'localhost',
     }
 }
 
